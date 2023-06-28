ab1803dbb7ea4914660f8eb60fb40491a5f24fca
pushpraj-singh <88105933+singh-rajps@users.noreply.github.com>
2023-06-28 09:54:06 +0530

    I added some Assiment Tasks And I will Going to do that

---
+++ javascript-class/j3.js
@@ -0,0 +1,37 @@
+//Logical Operators
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+
+//Lets check c and Greatest
+
+console.log(c > b && c > a)
+console.log(b > c && b > a);
+console.log(a > b && a > c);
+
+
+let x = true
+let y = false
+
+console.log(x && y);
+
+console.log( x || y);
+console.log(x && y);
+
+console.log(!x || !y);
+
+//Increament/Decrement Operators
+
+let count = 0 ;
+console.log(count++);
+console.log(++count);
+console.log(count);
+console.log(--count);
+console.log(count--);
+
+
+
+//Ternary Operators
+count > 0 ? console.log('The Num is +ve'):
